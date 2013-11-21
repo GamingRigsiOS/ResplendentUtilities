4c9dca656a72271655b2488cbc7701b4f5d005b1
BenMaer <ben@resplendent.co>
2013-11-21 17:59:47 -0500

    suppressed RUCompatability.m warning

--- ResplendentUtilities/Misc/RUCompatability.m
+++ ResplendentUtilities/Misc/RUCompatability.m
@@ -31,7 +31,10 @@
     }
     else
     {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         [self setMinimumFontSize:minimumFontSizeScaleFactor];
+#pragma clang diagnostic pop
     }
 }
 
